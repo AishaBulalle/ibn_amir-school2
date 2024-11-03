9a573e9115fba61913dc830363c9fb440a2d9c73
AishaBulalle <124045374+AishaBulalle@users.noreply.github.com>
2024-11-03 16:14:17 +0100

    updated homePage.tsx and homePage.css

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,22 +3,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 import videoFile from '../assets/ibnamir-video.mp4'; // Import the video file
+import quranImage from '../assets/quran.webp'; // Import Quran image
+import arabicImage from '../assets/arabic.webp'; // Import Arabic image
+import islamicStudiesImage from '../assets/islamic studies.webp'; // Import Islamic Studies image
 
 const HomePage = () => {
   return (
     <div className="homepage-container">
       <header className="homepage-header">
-        <h1>Ibn Amer</h1>
+        <h1>🕌Ibn Amir</h1>
         <Link to="/login" className="login-button">
           Login
         </Link>
       </header>
 
       <section className="homepage-section">
-        <h2>Welcome to Ibn Amer School</h2>
+        <h2>🕌Welcome to Ibn Amir🕌</h2>
         <p>
           Empowering students with knowledge, values, and skills for life. Ibn
-          Amir School is dedicated to providing students with a balanced
+          Amir school is dedicated to providing students with a balanced
           education that combines academic excellence and moral values.
         </p>
       </section>
@@ -31,10 +34,32 @@ const HomePage = () => {
       </section>
 
       <section className="courses-section">
-        <h3>Courses Offered:</h3>
-        <p>Quran Recitation</p>
-        <p>Arabic Language</p>
-        <p>Islamic Studies</p>
+        <h3>Courses</h3>
+        <div className="course-container">
+          <div className="course-box">
+            <h4>Quran Recitation</h4>
+            <div className="course-image">
+              <img src={quranImage} alt="Quran Recitation" />
+            </div>
+            <p>
+              Learn to recite the Quran with proper Tajweed and understanding.
+            </p>
+          </div>
+          <div className="course-box">
+            <h4>Arabic Language</h4>
+            <div className="course-image">
+              <img src={arabicImage} alt="Arabic Language" />
+            </div>
+            <p>Explore the beauty of the Arabic language and its structure.</p>
+          </div>
+          <div className="course-box">
+            <h4>Islamic Studies</h4>
+            <div className="course-image">
+              <img src={islamicStudiesImage} alt="Islamic Studies" />
+            </div>
+            <p>Understand the principles and teachings of Islam.</p>
+          </div>
+        </div>
       </section>
 
       <footer className="footer">
