87ad40d3c515cabc84e6df825b1b19f8b19b512c
AishaBulalle <124045374+AishaBulalle@users.noreply.github.com>
2024-11-07 11:57:55 +0100

    made env file with the database password and updated the db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
+//const db = mysql.createConnection({
+//host: 'localhost',
+//user: 'root',
+// password: 'aisha123',
+//database: 'ibn_amir_school2',
+//});
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'aisha123',
-  database: 'ibn_amir_school2',
+  host: process.env.DB_HOST || 'localhost', // Default to localhost for local development
+  user: process.env.DB_USER || 'root', // Default to root for local development
+  password: process.env.DB_PASS || process.env.DB_PASSWORD, // Get the password from the environment variable
+  database: process.env.DB_NAME || 'ibn_amir_school2', // Default to your database name
 });
 
 db.connect((err) => {
